6785cc6584f0d5673e5896c995fb3e0681fd0756
koalumpur <olka.serova@gmail.com>
2023-06-16 18:38:56 +0300

    Added code from stash Rewrote task2 re: comments

--- task2.rb
+++ task2.rb
@@ -1,42 +1,38 @@
 class MyFirstClass
-    def initialize
-    end
+  def initialize(a)
+    @var = a
+  end
+        
+  def var
+    @var
+  end
+        
+  def var=(number)
+    @var = number
+  end
 end
 
 class MySecondClass < MyFirstClass
 end
 
-f = MyFirstClass.new
-s = MySecondClass.new
+puts MySecondClass.superclass
+puts MySecondClass.superclass.superclass
+
+f = MyFirstClass.new(1)
+s = MySecondClass.new(2)
 puts f.class
 puts s.class
     
-    puts f.class == MyFirstClass
-    puts s.class == MySecondClass
-    puts MySecondClass.superclass
-    puts MySecondClass.superclass.superclass
-    
-    class MyFirstClass
-        def initialize(a)
-            @var = a
-        end
+puts f.class == MyFirstClass
+puts s.class == MySecondClass
+
+inst = MyFirstClass.new(3)
+puts inst.class
+puts inst.var
+puts inst.var = 4
         
-        def var
-            @var
-        end
-        
-        def var=(number)
-            @var = number
-        end
-    end
-    
-    inst = MyFirstClass.new(3)
-    puts inst.class
-    puts inst.var
-    puts inst.var=(5)
-        
-        inst2 = MySecondClass.new(2)
-        puts inst2.class
-        puts inst2.var
-        puts inst2.var=(4)
+inst2 = MySecondClass.new(5)
+puts inst2.class
+puts inst2.var
+puts inst2.var = 6
         
