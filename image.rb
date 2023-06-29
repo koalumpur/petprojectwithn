a5aa8edcf4ccb41362ca19b5848633b201b910b1
koalumpur <olka.serova@gmail.com>
2023-06-29 22:56:29 +0300

    Added size and sensitive content check for class Avatar; added new variable b (class Avatar)

--- image.rb
+++ image.rb
@@ -19,4 +19,16 @@ puts a.class
 puts a.file_name(@name, @type)
   
 class Avatar < Image
+  def initialize(type, size, name, url)
+    super
+    raise 'Too big Image!' if size > 10_000
+    raise "My eyes! My eyes! Please choose an appropriate picture!" if @sensitive_content == true
+  end
 end
+
+b = Avatar.new("png", 300, "loli", "www.site2.com")
+#c = Avatar.new("png", 30000, "pop", "www.site3.com")
+#d = Avatar.new("png", 100, "lip", "www.site4.com")
+
+puts b.class#, c.class, d.class
+puts b.file_name(@name, @type)#, c.file_name(@name, @type), d.file_name(@name, @type)
